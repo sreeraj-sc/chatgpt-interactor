f818a713df1b088143c56a5417254cdc345277b9
sreeraj-sc <sreeraj.s.c.vfc@gmail.com>
2023-02-05 18:37:28 +0530

    chat gpt ineractor

---
+++ chatgpt.py
@@ -0,0 +1,21 @@
+import openai
+def interact_with_chatbot(prompt):
+    openai.api_key = "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"
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        print("Chatbot: Goodbye!")
+        break
+    response = interact_with_chatbot(user_input)
+    print("Chatbot: " + response)
